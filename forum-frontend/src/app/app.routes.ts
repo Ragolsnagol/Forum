c9ab7179394b9a488c288c798921e738958a480c
Ryan Majeski <ryanmajeski@gmail.com>
2024-02-20 14:27:25 -0500

    PageNotFound added

--- forum-frontend/src/app/app.routes.ts
+++ forum-frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { TopicListComponent } from './topic/topic-list/topic-list.component';
 import { DashboardPageComponent } from './dashboard/dashboard-page/dashboard-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'topics', component: TopicListComponent },
   { path: 'dashboard', component: DashboardPageComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
