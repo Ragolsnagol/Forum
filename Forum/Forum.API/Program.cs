3e62cd2a359044fcbfa1e92029494082966ffb20
Ryan Majeski <ryanmajeski@gmail.com>
2024-03-11 17:26:28 -0400

    Add logout endpoint

--- Forum/Forum.API/Program.cs
+++ Forum/Forum.API/Program.cs
@@ -1,5 +1,6 @@
 using Forum.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -16,7 +17,18 @@ builder.Services.AddIdentityApiEndpoints<IdentityUser>()
     .AddEntityFrameworkStores<ForumDbContext>();
 
 var app = builder.Build();
+
 app.MapIdentityApi<IdentityUser>();
+app.MapPost("/logout", async (SignInManager<IdentityUser> signInManager, [FromBody] object empty) =>
+{
+    if (empty != null)
+    {
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+    }
+    return Results.Unauthorized();
+})
+.RequireAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
